5d1af34eea47081929c0651eb539f91d88c6c236
Pratik33 <pratikmothe02@gmail.com>
2024-02-02 10:22:45 +0530

    init : repo init

---
+++ Namaste-React/EP-03-Laying-the-foundation/App.js
@@ -0,0 +1,23 @@
+import  React  from 'react';
+import  ReactDOM  from 'react-dom';
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World !  from React !"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child-one" }, [
+    React.createElement("h1", {id:"heading"}, "Welcome to Namaste React"),
+    React.createElement("p", "", "This is one of the best course !"),
+  ]),
+  React.createElement("div", { id: "child-two" }, [
+    React.createElement("h2", "", "We are using parcel now !!"),
+    React.createElement("p", "", "Removed React CDN links and using React and react-DOM package "),
+  ])
+]);
+
+root.render(parent);
